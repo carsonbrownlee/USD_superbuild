9286110875b2532e091645c5071be235653f95f3
sherholz <sebastian.herholz@gmail.com>
2021-04-08 11:49:47 +0200

    [ptex] raised version to 2.3.1
    Added PTEX_VER define to the build script since the CMake script of PTex is broken.

--- cmake/Ptex.cmake
+++ cmake/Ptex.cmake
@@ -2,13 +2,15 @@
 ## Ptex
 ##
 
-set(Ptex_ARGS "")
+set(Ptex_ARGS
+-DPTEX_VER=v2.3.1
+)
 
 set(Ptex_DEPENDENCIES "")
 
 build_component(
   NAME Ptex
-  VERSION "v2.1.28"
+  VERSION "v2.3.1"
   URL https://github.com/wdas/ptex
   BUILD_ARGS ${Ptex_ARGS}
   DEPENDS_ON ${Ptex_DEPENDENCIES}
